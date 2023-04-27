1c9d06efdd9a89998b82e76f15fabc2a5faf2537
SMirza <shokhidamir@gmail.com>
2023-04-27 23:18:07 +0200

    codingBat task solutions

--- src/codingBat_Practices/Part02/LastDigit.java
+++ src/codingBat_Practices/Part02/LastDigit.java
@@ -1,6 +1,26 @@
 package codingBat_Practices.Part02;
 
 public class LastDigit {
+
+    public static boolean lastDigit(int a, int b, int c) {
+
+        // doing number % 10 will give the last digit of the number
+        a = a % 10;
+        b %= 10;
+        c %= 10;
+
+        return a == b || b == c || c == a;
+
+    }
+// 3 % 10 => 3 3:10 est
+
+
+
+
+
+
+
+
     /*
     Given three ints, a b c, return true if two or more of them have the same rightmost digit. The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.
 
--- src/codingBat_Practices/Part02/TwoAsOne.java
+++ src/codingBat_Practices/Part02/TwoAsOne.java
@@ -1,6 +1,18 @@
 package codingBat_Practices.Part02;
 
 public class TwoAsOne {
+
+    public boolean twoAsOne(int a, int b, int c) {
+
+        boolean one = a + b == c;
+        boolean two = a + c == b;
+        boolean three = b + c == a;
+
+        return one || two || three;
+
+    }
+
+
     /*
     Given three ints, a b c, return true if it is possible to add two of the ints to get the third.
 
