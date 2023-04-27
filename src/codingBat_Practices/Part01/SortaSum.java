8c806f14d06bc69d31a3858709729bfab0cf8e75
SMirza <shokhidamir@gmail.com>
2023-04-27 23:10:37 +0200

    codingBat task solutions

--- src/codingBat_Practices/Part01/SortaSum.java
+++ src/codingBat_Practices/Part01/SortaSum.java
@@ -12,19 +12,27 @@ public class SortaSum {
         return sum;
     }
 
-// public int sortaSum(int a, int b) {
-//   int sum = a + b;
+    public static void main(String[] args) {
+        System.out.println(sortaSum(3, 4)); // 7
+        System.out.println(sortaSum(9, 4)); // 20
+        System.out.println(sortaSum(10, 11)); // 21
+    }
 
-//   return sum >= 10 && sum <= 19 ? 20 : sum;
-// }
 }
 
 
 /*
-Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+     public static int sortaSum(int a, int b) {
+     int sum = a + b;
+     return sum >= 10 && sum <= 19 ? 20 : sum;
+     }
 
 
-sortaSum(3, 4) → 7
-sortaSum(9, 4) → 20
-sortaSum(10, 11) → 21
+
+     Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive are forbidden, so in that case just return 20.
+
+
+    sortaSum(3, 4) → 7
+    sortaSum(9, 4) → 20
+    sortaSum(10, 11) → 21
  */
